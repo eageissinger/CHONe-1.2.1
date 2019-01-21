c0457f2e12d5598ba550f5c231119f2b80f6ca49
eageissinger <eageissinger@mun.ca>
2019-01-21 15:53:38 -0330

    condition models revised

--- code/condition-exp-pt2.R
+++ code/condition-exp-pt2.R
@@ -1,7 +1,7 @@
 #----Overwinter Condition Analysis-----
 
 #load working directory
-setwd("C:/Users/Emilie/Dropbox/Thesis/Research/CHONe-1.2.1/")
+setwd("C:/Users/USER/Documents/Research/CHONe-1.2.1/")
 
 #----load data-----
 condition<-read.csv("./data/data-working/condition-exp.csv",header=TRUE)
@@ -13,29 +13,21 @@ tanks<-read.csv("./data/data-working/tank-assignments-exp.csv")
 # ----load pacakges -----
 library(car)
 library(MASS)
+library(lattice)
 library(lubridate)
 library(tidyr)
 library(stringr)
 library(dplyr)
 library(ggplot2)
 library(stats)
-library(lattice)
-library(ggpmisc)
 library(lme4)
 library(nlme)
-library(numDeriv)
-library(glmmTMB)
-library(psych)
-library(betareg)
 library(lmtest)
 library(rcompanion)
 library(multcompView)
 library(emmeans)
-library(grid)
+library(betareg)
 
-
-
-#source("ggplot_smooth_func.R")
 
 # --- check data -----
 #length-weight
@@ -268,7 +260,7 @@ livecond$tank<-as.factor(livecond$tank)
 
 lc<-livecond%>%
   group_by(tank,julian_date,ration,size)%>%
-  summarise(kavg=mean(k),sdk=sd(k),sek=sdk/sqrt(n()))%>%
+  summarise(kavg=mean(k),sdk=sd(k),sek=sdk/sqrt(n()),temp=mean(temperature))%>%
   data.frame()
 lc$tank<-as.factor(lc$tank)
 str(lc)
@@ -284,85 +276,27 @@ lc<-lc%>%
            julian_date==114)
 
 # ---- LC Models ----
-lc.m1<-lmer(kavg~ration*size*julian_date+(ration|tank)+(size|tank),data=lc)
+lc.m1<-lm(kavg~ration*size*julian_date,data=lc)
 lc.m1
 plot(lc.m1)
 summary(lc.m1)
-av1<-anova(lc.m1)
-anova(lc.m1)
-resid<-resid(lc.m1)
-SSresid<-sum(resid^2)
-resid
-SSresid
-SStotal<-sum(av1$`Sum Sq`)+SSresid
-SStotal
-LR<-SSresid/SStotal
-LR
+Anova(lc.m1,type="III")
 
-ggplot(lc.m1,aes(x=fitted(lc.m1),y=resid(lc.m1)))+geom_point()
-hist(resid(lc.m1))
-qqnorm(resid(lc.m1))
-
-lc.m2<-glmer(kavg~ration*size*julian_date+(ration|tank)+(size|tank),
-             data=lc,family = Gamma(link = "log"))
+# get rid of three way interaction
+lc.m2<-lm(kavg~(ration+size+julian_date)^2,data=lc)
 lc.m2
 plot(lc.m2)
+summary(lc.m2)
+Anova(lc.m2,type="III")
+
 hist(resid(lc.m2))
-qqnorm(resid(lc.m2))
-summary(lc$kavg)
-
-# ---- LC model Beta regression ----
-lc.m3<-glmmTMB(kavg~ration*size*julian_date+(ration|tank)+(size|tank),
-        data=lc,family = list(family="beta",link="logit"))
-plot(lc.m3)
-
-lc.m4<-betareg(kavg~ration*size*julian_date,data=lc)
-lc.m4$model
-plot(lc.m4)
-hist(resid(lc.m4))
-qqnorm(resid(lc.m4))
-
-joint_tests(lc.m4)
-lrtest(lc.m4)
-summary(lc.m4)
-plot(x=fitted(lc.m4),y=residuals(lc.m4))
-marginal<-emmeans(lc.m4,~ration)
-pairs(marginal,adjust="tukey")
-exp(logLik(lc.m4))
-
-lc.m4.5<-betareg(kavg~ration*julian_date,data=lc)
-plot(lc.m4.5)
-hist(resid(lc.m4.5))
-qqnorm(resid(lc.m4.5))
-
-joint_tests(lc.m4.5)
-lrtest(lc.m4.5)
-summary(lc.m4.5)
-plot(x=fitted(lc.m4.5),y=residuals(lc.m4.5))
-marginal<-emmeans(lc.m4.5,~ration)
-pairs(marginal,adjust="tukey")
-exp(logLik(lc.m4.5))
-exp(logLik(lc.m4))
-lc.m5<-betareg(kavg~size+ration*julian_date,data=lc)
-plot(lc.m4)
-hist(resid(lc.m5))
-qqnorm(resid(lc.m5))
-joint_tests(lc.m5)
-lrtest(lc.m5)
-summary(lc.m5)
-
-logLik(lc.m4)
-logLik(lc.m5)
-exp(logLik(lc.m4))
-exp(logLik(lc.m5))
-#plot without size
 
 limitse<-aes(ymin=kavg-sek,ymax=kavg+sek)
 lc%>%
   rename(Ration=ration)%>%
-  ggplot(aes(x=julian_date,y=kavg,colour=Ration))+
+  ggplot(aes(x=julian_date,y=kavg,colour=Ration,linetype=size))+
   geom_jitter(aes(shape=Ration,colour=Ration,fill=Ration),size=3)+
-  stat_smooth(method="lm",linetype='dashed',size=1,se=FALSE)+
+  stat_smooth(method="lm",se=FALSE)+
   geom_errorbar(limitse,width=1)+
   theme_bw(base_rect_size = 2)+
   ylab("Condition factor (K)")+xlab("Day of experiment")+
@@ -381,57 +315,6 @@ lc%>%
   theme(plot.margin = unit(c(0.75,0.75,0.75,0.75),"cm"))
 
 
-livecond.small<-lc%>%
-  filter(size=="small")%>%
-  data.frame()
-
-livecond.large<-lc%>%
-  filter(size=="large")%>%
-  filter(julian_date!=80)%>%
-  data.frame()
-
-
-#small
-livecond.small.plot<-ggplot(livecond.small,aes(x=julian_date,y=kavg,colour=ration))+
-  geom_jitter(aes(shape=ration,colour=ration,fill=ration),size=3)+
-  stat_smooth(method="lm",linetype='dashed',size=1,se=FALSE)+
-  geom_errorbar(limitse,width=1)+
-  xlab("Day of Experiment")+ylab("Condition factor (K)")+
-  theme_classic()+
-  theme(axis.text.x=element_text(size=20,face='bold'))+
-  theme(axis.text.y=element_text(size=20,face='bold'))+
-  theme(legend.title=element_text(size=18,face='bold'))+
-  ylim(0.6,1.0)+
-  theme(axis.title=element_text(size=20))+
-  theme(plot.title=element_text(size=20,face="bold",hjust=0.5))+
-  theme(legend.key=element_blank())+
-  theme(legend.position=c(0.9,0.19))+
-  scale_colour_manual(values=c('grey0','grey25','grey39','grey64'))+
-  scale_fill_manual(values=c('grey0','grey25','grey39','grey64'))+
-  scale_shape_manual(values=c(22:25))
-livecond.small.plot
-
-#large
-livecond.large.plot<-ggplot(livecond.large,aes(x=julian_date,y=kavg,colour=ration))+
-  geom_point(aes(shape=ration,colour=ration,fill=ration),size=3)+
-  stat_smooth(method="lm",linetype='dashed',size=1,se=FALSE)+
-  geom_errorbar(limitse,width=1)+
-  theme_classic()+
-  ylim(0.6,1.0)+
-  theme(axis.title=element_text(size=20))+
-  theme(plot.title=element_text(size=20,face="bold",hjust=0.5))+
-  theme(legend.key=element_blank())+
-  theme(axis.title.x=element_blank())+
-  theme(axis.title.y=element_blank())+
-  theme(axis.text.x=element_text(size=20,face='bold'))+
-  theme(axis.text.y=element_text(size=20,face='bold'))+
-  theme(legend.title=element_text(size=18,face='bold'))+
-  theme(legend.position=c(0.9,0.19))+
-  scale_colour_manual(values=c('grey0','grey25','grey39','grey64'))+
-  scale_fill_manual(values=c('grey0','grey25','grey39','grey64'))+
-  scale_shape_manual(values=c(22:25))
-livecond.large.plot
-
 # ----- Final Condition analysis ------
 summary(fishcond)
 dim(fishcond)
@@ -446,23 +329,17 @@ View(finalcond)
 
 finalK<-finalcond%>%
   group_by(tank,ration,size,percent)%>%
-  summarise(dry=mean(kdry),wet=mean(kwet),
+  summarise(dry=mean(kdry),wet=mean(kwet),hsi=mean(HSI),
             sedry=(sd(kdry)/sqrt(n())),
-            sewet=(sd(kwet)/sqrt(n())))
+            sewet=(sd(kwet)/sqrt(n())),
+            sehsi=sd(HSI/sqrt(n())))
 View(finalK)
-# add fish # for each tank
-finalcond<-finalcond%>%
-  group_by(tank)%>%
-  mutate(fish=seq(1:9))
+
 
 # Dry condition model analysis
 
-ggplot(data=finalcond,aes(x=percent,y=kdry))+geom_point()
-ggplot(data=finalcond,aes(x=percent,y=kdry,colour=size))+geom_point()
-
-
 #model dry condition
-d.m0<-lmer(kdry~percent*size+(percent|tank)+(size|tank),data=finalcond)
+d.m0<-lm(dry~percent*size,data=finalK)
 plot(d.m0)
 hist(resid(d.m0))
 qqnorm(resid(d.m0))
@@ -472,47 +349,33 @@ exp(logLik(d.m0))
 
 # GLMM
 
-d.m1<-glmer(kdry~percent*size+(percent|tank)+(size|tank),data=finalcond,
+d.m1<-glm(dry~percent*size,data=finalK,
             family=Gamma(link = "log"))
 d.m1
 summary(d.m1)
 plot(d.m1)
 hist(resid(d.m1))
 qqnorm(resid(d.m1))
-anova(d.m1)
 Anova(d.m1,type = "III")
 exp(logLik(d.m1))
 
-d.m2<-glmer(kdry~percent+size+(percent|tank)+(size|tank),data=finalcond,
+d.m2<-glm(dry~percent+size,data=finalK,
             family=Gamma(link = "log"))
 d.m2
 summary(d.m2)
 plot(d.m2)
 hist(resid(d.m2))
 qqnorm(resid(d.m2))
-plot(x=fitted(d.m2),y=resid(d.m2))
-anova(d.m2)
-Anova(d.m2,type = "III")
-exp(logLik(d.m1))
-exp(logLik(d.m2))
+Anova(d.m2,type="III")
 
-# use averaged data
+# Generalized Least Sqaures
 
-
-d.m3<-lm(dry~percent*size,data=finalK)
+d.m3<-gls(dry~percent*size,data=finalK)
 plot(d.m3)
 hist(resid(d.m3))
 qqnorm(resid(d.m3))
-plot(x=fitted(d.m3),y=resid(d.m3))
-logLik(d.m3)
 
-d.m4<-glm(dry~percent*size,data=finalK,family=Gamma(link = "log"))
-plot(d.m4)
-hist(resid(d.m4))
-qqnorm(resid(d.m4))
-plot(x=fitted(d.m4),y=resid(d.m4))
-exp(logLik(d.m4))
-
+# model d.m2, glm with gamma distribution and loglink
 
 finalK%>%
   mutate(percent_adj=percent-.05)%>%
@@ -537,7 +400,7 @@ finalK%>%
 
 # Wet condition factor
 
-w.m1<-lmer(kwet~percent*size+(percent|tank)+(size|tank),data=finalcond)
+w.m1<-lm(wet~percent*size,data=finalK)
 w.m1
 summary(w.m1)
 plot(w.m1)
@@ -546,23 +409,13 @@ qqnorm(resid(w.m1))
 Anova(w.m1,type = "III")
 
 #GLMM
-w.m2<-glmer(kwet~percent*size+(percent|tank)+(size|tank),data=finalcond,
+w.m2<-glm(wet~percent*size,data=finalK,
             family = Gamma(link = "log"))
 plot(w.m2)
 hist(resid(w.m2))
 qqnorm(resid(w.m2))
 
-# use averaged data
-w.m3<-lm(kwet~percent*size,data=finalK)
-plot(w.m3)
-hist(resid(w.m3))
-
-
-w.m4<-glm(kwet~percent*size,data=finalK,family=Gamma(link = "log"))
-plot(w.m4)
-hist(resid(w.m4))
-qqnorm(resid(w.m4))
-
+#use dry condition for results (proven to be more reliaable)
 
 # ----- Day 0 HSI -------
 d0HSI<-day0%>%
@@ -574,26 +427,14 @@ day0%>%
 
 # ---- HSI -----
 
-hsi.m1<-lmer(HSI~percent*size+(percent|tank)+(size|tank),data=finalcond)
+hsi.m1<-lm(hsi~percent*size,data=finalK)
 hsi.m1
-summary(hsi.m1)
 plot(hsi.m1)
+summary(hsi.m1)
+
 hist(resid(hsi.m1))
 qqnorm(resid(hsi.m1))
 Anova(hsi.m1,type = "III")
-
-hsi.m2<-lmer(HSI~percent+(percent|tank),data=finalcond)
-plot(hsi.m2)
-hist(resid(hsi.m2))
-qqnorm(resid(hsi.m2))
-summary(hsi.m2)
-Anova(hsi.m2,type = "III")
-
-hsi.m3<-lmer(HSI~size+(size|tank),data=finalcond)
-plot(hsi.m3)
-hist(resid(hsi.m3))
-qqnorm(resid(hsi.m3))
-Anova(hsi.m3,type = "III")
 
 
 # Model 1 is best
@@ -602,16 +443,24 @@ Anova(hsi.m3,type = "III")
 # use terminated only
 unique(finalcond$julian_date)
 endcond<-finalcond%>%
-  filter(julian_date==80 | julian_date == 84 | julian_date == 114)
+  filter(julian_date==80 | julian_date == 84 | julian_date == 114)%>%
+  group_by(tank,ration,size,percent)%>%
+  summarise(hsi=mean(HSI),hsise=sd(HSI)/sqrt(n()))
 
-hsi.m4<-lmer(kdry~percent*size+(percent|tank)+(size|tank),data=endcond)
-plot(hsi.m4)
-hist(resid(hsi.m4))
+
+hsi.m2<-lm(hsi~percent*size,data=endcond)
+plot(hsi.m2)
+hist(resid(hsi.m2))
 qqnorm(resid(hsi.m4))
 
-hsi.m5<-glmer(kdry~percent*size+(percent|tank)+(size|tank),data=endcond,
+hsi.m3<-glm(hsi~percent*size,data=endcond,
               family = Gamma(link="log"))
-plot(hsi.m5)
+plot(hsi.m3)
+hist(resid(hsi.m3))
+
+
+# hsi.m1! figure out how to interpret the interaction!
+
 # ---- Survival -----
 
 #Cacluate survival rate by tank
@@ -621,7 +470,8 @@ survival<-tank_survival%>%
   mutate(survival=(number/9))%>%
   ungroup()%>%
   group_by(tank,size,ration,julian_date)%>%
-  summarise(perc_surv=mean(survival))
+  summarise(perc_surv=mean(survival))%>%
+  filter(perc_surv!=0)
 
 # Visualize data
 survival%>%
@@ -632,11 +482,29 @@ survival%>%
   ggplot(aes(x=julian_date,y=perc_surv,linetype=ration))+geom_smooth()
 
 # ----- survival models ----
+
+# GLS
+s.m1<-gls(perc_surv~size*ration*julian_date,data=survival)
+plot(s.m1)
+
+s.m2<-lm(perc_surv~size*ration*julian_date,data=survival)
+plot(s.m2)
+
+s.m3<-glm(perc_surv~size*ration*julian_date,data=survival,family=Gamma(link="log"))
+plot(s.m3)
+glimpse(survival)
+survival$tank<-as.factor(survival$tank)
+s.m4<-glmer(perc_surv~size*ration*julian_date+(ration|tank)+(size|tank),
+            family=Gamma(link="log"),data=survival)
+plot(s.m4)
+
+
 # split into increments of 30 days
 # survival at day 20, 40, 60, 80
 S<-survival%>%
-  filter(julian_date==0 | julian_date==20 | julian_date==40 |
-           julian_date ==60 | julian_date ==80)%>%
+  filter(julian_date==0 | julian_date==23 | 
+           julian_date==46 | 
+           julian_date ==69| julian_date == 114)%>%
   mutate(percent=0.0)%>%
   mutate(percent=replace(percent,ration=="0.5%",0.5))%>%
   mutate(percent=replace(percent,ration=="1.0%",1.0))%>%
@@ -644,27 +512,39 @@ S<-survival%>%
 typeof(S$julian_date)
 S$julian_date<-as.factor(S$julian_date)
 
-s.m1<-lm(perc_surv~ration*size*factor(julian_date),data=S)
-plot(s.m1)
-hist(resid(s.m1))
-qqnorm(resid(s.m1))
-anova(s.m1)
-Anova(s.m1,type = "III")
-logLik(s.m1)
-exp(logLik(s.m1))
-exp(logLik(s.m2))
+s.m5<-lm(perc_surv~ration+size+factor(julian_date),data=S)
+plot(s.m5)
+hist(resid(s.m5))
+qqnorm(resid(s.m5))
+Anova(s.m5,type = "III")
 
-s.m2<-lmer(perc_surv~ration*size*factor(julian_date)+(size|tank)+
-              (ration|tank),data=S)
+s.m6<-glm(perc_surv~ration+size+factor(julian_date),data=S,
+          family=Gamma(link = "log"))
+plot(s.m6)
+hist(resid(s.m6))
+Anova(s.m6,type="III")
 
-plot(s.m2)
-hist(resid(s.m2))
-qqnorm(resid(s.m2))
+S<-survival%>%
+  filter(julian_date==0 | julian_date==30 | 
+           julian_date==60 | 
+           julian_date ==90| julian_date == 114)%>%
+  mutate(percent=0.0)%>%
+  mutate(percent=replace(percent,ration=="0.5%",0.5))%>%
+  mutate(percent=replace(percent,ration=="1.0%",1.0))%>%
+  mutate(percent=replace(percent,ration=="2.0%",2.0))
+typeof(S$julian_date)
+S$julian_date<-as.factor(S$julian_date)
 
-s.m3<-glmer(perc_surv~ration*size*julian_date+(size|tank)+
-              (ration|tank),data=survival,family = Gamma(link = 'log'))
+s.m5<-lm(perc_surv~ration+size+factor(julian_date),data=S)
+plot(s.m5)
+hist(resid(s.m5))
+qqnorm(resid(s.m5))
+Anova(s.m5,type = "III")
 
-
+s.m6<-glm(perc_surv~ration+size+factor(julian_date),data=S,
+          family = Gamma(link = "log"))
+plot(s.m6)
+hist(resid(s.m6))
 survival%>%
   filter(size=="small")%>%
   rename(Ration=ration)%>%
