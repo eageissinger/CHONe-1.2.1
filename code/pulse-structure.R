51c9fd32c3a177353d8ec4da09728ce28b9dec89
eageissinger <eageissinger@mun.ca>
2019-01-22 15:05:59 -0330

    fixed typo in file name

--- code/pulse-structure.R
+++ code/pulse-structure.R
@@ -1,5 +1,5 @@
 # --- set working directory ----
-setwd("C:/Users/geissingere/Documents/CHONe-1.2.1-master/")
+setwd("C:/Users/USER/Documents/Research/CHONe-1.2.1/")
 
 # --- load packages ----
 library(ggplot2)
@@ -34,6 +34,7 @@ catch<-select(catch,-Date)
 # --- combine length and trip data ----
 alldata<-left_join(length,trips)
 
+# ---- Part 1: Mixture Distributions ----
 # May
 # --- select data ----
 may2016 <- filter(alldata, age ==1 & year ==2016 & month ==5)
@@ -428,7 +429,8 @@ summarise(age1.2016,min(mmSL),max(mmSL))
 group2016<-select(age1.2016,mmSL)
 
 # convert to frequency table
-age1group<-mixgroup(group2016, breaks = c(0,seq(105,190,5),195),xname=NULL, k = NULL, usecondit = FALSE)
+age1group<-mixgroup(group2016, breaks = c(0,seq(105,190,5),195),xname=NULL, k = NULL, 
+                    usecondit = FALSE)
 
 # plot frequency data
 plot(age1group)
@@ -452,9 +454,10 @@ trip21<-bind_cols(fit1$parameters,fit1$se)%>%
 trip21<-mutate(trip21,dummy_pulse=rev(seq(1:nrow(trip21))))
 
 # store results in a dataframe
-mixtures<-bind_rows(trip10,trip12,trip13,trip14,trip15,trip16,trip17,trip18,trip19,trip20,trip21)
+mixtures<-bind_rows(trip10,trip12,trip13,trip14,trip15,trip16,trip17,trip18,trip19,trip20,trip21)%>%
+  mutate(cohort=year-1)
 
-# ---- assigning pulses ----
+# ---- Part 2: Estimating pulses ----
 age0<-alldata%>%
   filter(year == 2015 & age==0)%>%
   group_by(year,month,day,trip,age,pulse)%>%
@@ -467,8 +470,7 @@ age0$pulse<-as.integer(as.character(age0$pulse))
 age1<-mixtures%>%
   rename(mean=mu,pulse = dummy_pulse)%>%
   mutate(min=mean-sigma,max=mean+sigma,age=1)%>%
-  select(-sigma,-sigma.se,-mu.se,-pi,-pi.se)%>%
-  mutate(cohort=year-1)
+  select(-sigma,-sigma.se,-mu.se,-pi,-pi.se)
 
 #  combine age 0 and age 1 data frames
 pulse.growth<- bind_rows(age0,age1)
@@ -487,7 +489,7 @@ ggplot(pulse.growth,aes(x=date,y=mean,shape=factor(pulse)))+geom_point(size=2)+
   theme(axis.text.x=element_text(angle=40))
 
 # calculate total and pulse abundances
-# ---- age 0 and age 1 Totals -----
+# age 0 and age 1 Totals
 count0<-catch%>%
   filter(age==0 & year == 2015)%>%
   group_by(year,month,trip,age)%>%
@@ -504,7 +506,7 @@ count1<-catch%>%
   summarise(count=mean(count)) 
 
 # age 0 by pulse
-# ---- tables -----
+# tables
 catch2<-haul%>%
   select(year,month,trip,age,total_catch,total_measured,extrap_1,extrap_2,
          extrap_3,extrap_4,extrap_5,extrap_6,extrap_unknown,total)%>%
@@ -519,3 +521,112 @@ table1<-catch2%>%
   summarise(catch_per_haul=mean(catch_haul))
 
 age0pulse<-filter(table1,year==2015 & age ==0)
+
+
+# ---- Part 3: Pulse Assignments ----
+# make dataframe with min and max pulse ranges
+# use +/- standard deviation for "min" and "max"
+pulse.range<-mixtures%>%
+  group_by(year,month,day,trip,dummy_pulse,year,cohort)%>%
+  summarise(min=(mu-sigma),max=mu+sigma,sd=sigma)%>%
+  mutate(min_round=floor(min),max_round=ceiling(max))%>%# add rounded min and max to create an integer column. Min is rounded down, max is rounded up
+  ungroup()%>%
+  group_by(year,month,day)%>%
+  mutate(diff=(lag(min_round)-max_round)/2)%>% # split difference between the pulse groups
+  mutate(plusmax=floor(diff))%>% # round to prevent overlap
+  mutate(minusmin=floor(lead(diff)))%>% # round to prevent overlap
+  replace_na(list(plusmax=0,minusmin=0))%>% # change NAs to 0
+  mutate(min_final=min_round-minusmin,max_final=max_round+plusmax)%>% # add/subtract appropriate terms
+  mutate(lagmin=lag(min_final))%>% # create a lag column of min values to compare to max
+  mutate(max1=replace(max_final,lagmin==max_final,1))%>% # any point where lagmin and lagmax match, put in a 1
+  mutate(max1=replace(max1,max1==max_final,0))%>% # 0 for all others
+  mutate(max_final2=max_final-max1)%>% # subtract 1 from max final (gets rid of any remaining overlap)
+  select(-diff,-plusmax,-minusmin,-max_final,-lagmin,-max1)%>% #get rid of useless columns
+  mutate(adjustmax=ceiling(2*sd))%>% # adjust maximum value to be 2 SDs for pulse 1
+  mutate(adjustmax=replace(adjustmax,dummy_pulse!=1,0))%>% # applies the above function to only pulse 1
+  mutate(adjustmin=ceiling(2*sd))%>% # adjust maximum value to be 2 SDs for last pulse
+  mutate(adjustmin=replace(adjustmin,dummy_pulse!=max(dummy_pulse),0))%>% # applies above function to last pulse only
+  mutate(max_final3=adjustmax+max_final2,
+         min_final3=min_final-adjustmin)%>% # create final max and min columns
+  select(year,month,day,trip,dummy_pulse,year,cohort,min_final3,max_final3)%>% # select final columns
+  rename(min=min_final3,max=max_final3) # rename to be min and max
+# use min and max for each pulse to then create a dataframe with all length possibilities per pulse
+pulse.assign<-data.frame(trip=rep(pulse.range$trip,pulse.range$max-pulse.range$min+1),
+                         year=rep(pulse.range$year,pulse.range$max-pulse.range$min+1),
+                         month=rep(pulse.range$month,pulse.range$max-pulse.range$min+1),
+                         day=rep(pulse.range$day,pulse.range$max-pulse.range$min+1),
+                         cohort=rep(pulse.range$cohort,pulse.range$max-pulse.range$min+1),
+                         pulse=rep(pulse.range$dummy_pulse,pulse.range$max-pulse.range$min+1),
+                         mmSL=unlist(mapply(seq,pulse.range$min,pulse.range$max)))
+# add additinal info such as date, cohort, pulse so that it is present in dataframe
+#check the new dataframe
+View(pulse.assign)
+summary(pulse.assign)
+glimpse(pulse.assign)
+
+# assign pulses to age 1 length data
+# select age 1 cod
+age1length<-alldata%>%filter(age==1 & year ==2016)%>%select(-pulse,-Date)
+
+# assign pulses to subsetted age1 length data
+age1.length.pulse<-left_join(age1length,pulse.assign)
+View(age1.length.pulse)
+
+# check quality of ranges (frequency check)
+
+# ---- Part 4: Verify pulse assignment ----
+# replot pulses and calculate pulse abundances
+#  combine age 0 and  with revised age 1 data frame (age1.length.pulse)
+# structure new age1 data to match age 0 format
+age1.revised<-age1.length.pulse%>%
+  group_by(year,month,day,trip,age,pulse)%>%
+  summarise(mean=mean(mmSL),min=min(mmSL),max=max(mmSL))
+pulse.growth2<- bind_rows(age0,age1.revised)
+
+# visualize pulses
+pulse.growth2$date<-ymd(paste(pulse.growth2$year,pulse.growth2$month,pulse.growth2$day,sep="-"))
+
+ggplot(pulse.growth2,aes(x=date,y=mean,shape=factor(pulse)))+geom_point(size=2)+
+  geom_errorbar(aes(ymin=min,ymax=max),width=0)+
+  theme_bw()+
+  ggtitle("2015 Cohort")+
+  xlab("Date")+ylab("Standard length (mm)")+
+  scale_x_date(date_breaks="1 month",
+               date_labels="%b")+
+  theme(axis.text.x=element_text(angle=40))
+
+# Review total abundances
+View(count0)
+View(count1)
+
+# Abundance by pulse
+# age 0
+# tables
+View(age0pulse)
+age1pulse<-filter(table1,year==2016 & age ==1)
+View(age1pulse)
+
+# ---- Part 5: final pulse assignment ----
+# age1.length.pulse
+age1final<-age1.length.pulse %>%
+  mutate(pulse = replace(pulse, trip == 10 & pulse == 3, 4)) %>%
+  mutate(pulse = replace(pulse, trip == 10 & pulse == 2, 3)) %>%
+  mutate(pulse = replace(pulse, trip == 10 & pulse == 1, 2)) %>%
+  mutate(pulse = replace(pulse, trip == 12 & pulse == 3, 4)) %>%
+  mutate(pulse = replace(pulse, trip == 12 & pulse == 2, 3)) %>%
+  mutate(pulse = replace(pulse, trip == 12 & pulse == 1, 2)) %>%
+  mutate(pulse = replace(pulse, trip == 13 & pulse == 3, 4)) %>%
+  mutate(pulse = replace(pulse, trip == 13 & pulse == 2, 3))
+
+
+# combine updated age 1 length data with length data
+all.length <- alldata %>%
+  filter(age != 1 & year != 2016)
+all.length %>% filter(age == 1 & year == 2016)
+all.length$pulse<-as.character(all.length$pulse)
+age1final$pulse<-as.character(age1final$pulse)
+final<-bind_rows(all.length,age1final)
+glimpse(final)
+
+# export full dataset
+write.csv(final,"Newman-Sound-length.csv",row.names = FALSE)
