dd7a166535b6d6b4448a1e584a7b1424cea3a610
eageissinger <eageissinger@mun.ca>
2019-02-05 16:38:19 -0330

    updated data

--- code/data-updates.R
+++ code/data-updates.R
@@ -1,62 +1,67 @@
 # ----- Data Updates ------
-# add new data to length and count files
+# update/add new data to length and count files
 
 # ----- set working directory -----
-setwd("C:/Users/Emilie/Dropbox/Thesis/Research/CHONe-1.2.1/")
+setwd("C:/Users/USER/Documents/Research/CHONe-1.2.1/")
 
 # ----- load data ------
-lengthold<-read.csv("./data/data-working/newman-length-to2017.csv")
-lengthnew<-read.csv("./data/data-original/newman-length-july-2017.csv")
-catchold<-read.csv("./data/data-working/newman-catch-full.csv")
-catchnew<-read.csv("./data/data-original/newman-catch-2017.csv")
-
+ac0length<-read.csv("./data/data-working/newman-AC0-length.csv")
+ac0count<-read.csv("./data/data-working/newman-AC0-catch.csv")
+ac1length<-read.csv("./data/data-working/newman-AC1-length.csv")
+ac1count<-read.csv("./data/data-working/newman-AC1-catch.csv")
+trip.dates<-read.csv("./data/data-working/trip-dates-newman.csv")
 # ---- load packages ----
-library(dplyr)
+library(tidyverse)
 library(lubridate)
-library(stringr)
-
 
 # ---- check data -----
-summary(lengthold)
-str(lengthold)
-dim(lengthold)
-names(lengthold)
+summary(ac0length)
+summary(ac0count)
+summary(ac1length)
+summary(ac1count)
+summary(trip.dates)
 
-summary(lengthnew)
-str(lengthnew)
-dim(lengthnew)
-names(lengthnew)
+glimpse(ac0length)
+glimpse(ac0count)
+glimpse(ac1length)
+glimpse(ac1count)
+glimpse(trip.dates)
 
-summary(catchold)
-str(catchold)
-dim(catchold)
-names(catchold)
-
-summary(catchnew)
-str(catchnew)
-dim(catchnew)
-names(catchnew)
+# ---- combine years ----
+# age 0 and age 1 length
+ac1length$Pulse<-as.character(ac1length$Pulse)
+ac0length$Pulse<-as.character(ac0length$Pulse)
+length<-bind_rows(ac0length,ac1length)
+# age 0 and age 1 count
+count<-bind_rows(ac0count,ac1count)
 
 # ---- fix date on new data -----
 # length
-length1<-lengthnew%>%
+length1<-length%>%
   mutate(month=as.numeric(str_sub(Date,start=5,end=6)),
          day=as.numeric(str_sub(Date,start=7,end=8)))%>%
   rename(year=Year,julian.date=Julian.Date,site=Site,species=Species,
-         age=Age,trip=Trip)%>%
+         age=Age,trip=Trip,pulse=Pulse,notes=Notes)%>%
   select(-Time,-Date)
 # catch
-catch1<-catchnew%>%
+count1<-count%>%
   mutate(month=as.numeric(str_sub(Date,start=5,end=6)),
          day=as.numeric(str_sub(Date,start=7,end=8)))%>%
   rename(year=Year,julian.date=Julian.Date,site=Site,species=Species,age=Age,
-         count=Count,notes=Notes)%>%
+         count=Count,notes=Notes,trip=Trip)%>%
+  select(-Date)
+#trips
+trip1<-trip.dates%>%
+  mutate(year=as.numeric(str_sub(Date,start=1,end=4)),
+         month=as.numeric(str_sub(Date,start=5,end=6)),
+         day=as.numeric(str_sub(Date,start=7,end=8)))%>%
+  rename(trip=Trip)%>%
   select(-Date)
 
-lengthall<-bind_rows(lengthold,length1)           
-catchall<-bind_rows(catchold,catch1)
-summary(lengthall)
-summary(catchall)
+View(length1)
+View(count1)
+View(trip1)
 
-write.csv(lengthall,"./data/data-working/newman-length.csv",row.names=FALSE)
-write.csv(catchall,"./data/data-working/newman-catch.csv",row.names=FALSE)
+write.csv(length1,"./data/data-working/newman-length.csv",row.names=FALSE)
+write.csv(count1,"./data/data-working/newman-catch.csv",row.names=FALSE)
+write.csv(trip1,"./data/data-working/newman-trips.csv",row.names=FALSE)
